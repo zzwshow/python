4e8a8eec29ab9b9c160eb8fbc7fe09a03a85c516
zhangzhiwei <zhangzhiwei@cnaviationloan.com>
2018-03-12 15:05:19 +0800

    实现cms 前台登陆页面

--- xmbbs/apps/cms/views.py
+++ xmbbs/apps/cms/views.py
@@ -1,9 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint,views,render_template
 
-bp = Blueprint("cms",__name__,url_prefix="/cms")
-
+bp = Blueprint("cms",__name__,url_prefix="/cms")  #蓝图url
 
 @bp.route('/')
 def index():
 	return "cms index"
+
+#登陆类视图
+class LoginView(views.MethodView):
+
+	def get(self):
+		return render_template('cms/cms_login.html')
+
+	def post(self):
+		pass
+
+
+#类视图url 添加到蓝图url中
+bp.add_url_rule('/login/',view_func=LoginView.as_view('login'))
+
 
